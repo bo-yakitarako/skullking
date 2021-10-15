eb7408330ab497558dbbcf413ef1a8c9347ed535
bo-yakitarako <bo-yakitarako@users.noreply.github.com>
2021-10-16 03:44:02 +0900

    ちゃんとやったよ

--- client/modules/http.ts
+++ client/modules/http.ts
@@ -1,24 +1,36 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 
-export async function post<Res, Params>(path: string, params?: Params) {
-  return http('POST', path, params) as Promise<Res>;
+type Params = Record<string, string | number | boolean>;
+
+export async function post<Res>(path: string, params?: Params) {
+  return http<Res>('POST', path, params);
 }
 
-export async function get<Res, Params>(path: string, params?: Params) {
-  return http('GET', path, params) as Promise<Res>;
+export async function get<Res>(path: string, params?: Params) {
+  return http<Res>('GET', path, params);
 }
 
-async function http<Res, Params>(
+async function http<Res>(
   method: 'GET' | 'POST',
   path: string,
   params?: Params,
 ) {
-  const headers = {
-    Accept: 'application/json',
-    'Content-Type': 'application/json',
-  };
-  const body = JSON.stringify(params);
-  const fetched = await fetch(path, { method, headers, body });
+  let requestConfig: RequestInit = { method };
+  if (method === 'POST') {
+    const headers = {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    };
+    const body = JSON.stringify(params);
+    requestConfig = { ...requestConfig, headers, body };
+  }
+  if (method === 'GET' && typeof params !== 'undefined') {
+    const getParams = Object.keys(params).map((key) => `${key}=${params[key]}`);
+    if (getParams.length > 0) {
+      path += `?${getParams.join('&')}`;
+    }
+  }
+  const fetched = await fetch(path, requestConfig);
   const response: Res = await fetched.json();
   return response;
 }
