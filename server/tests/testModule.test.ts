cbf973bc7ef3c84fc74d2e23fe69a37661a02554
bo-yakitarako <bo-yakitarako@users.noreply.github.com>
2021-10-23 17:42:46 +0900

    よっしゃ！

--- server/tests/testModule.test.ts
+++ server/tests/testModule.test.ts
@@ -2,7 +2,7 @@ import { add } from '../testModule';
 
 describe.each([
   [1, 1, 2],
-  [4, -2, 1],
+  [4, -2, 2],
   [-12, -34, -46],
 ])('add(%i, %i)', (a, b, expected) => {
   test(`足したら${expected}になるはずだぞぉ？`, () => {
