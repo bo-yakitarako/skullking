75b36c2204043dfd9ebd807c4c4920aa45636655
bo-yakitarako <bo-yakitarako@users.noreply.github.com>
2021-10-23 17:25:47 +0900

    Action変えてテストあえて落とすよ

--- server/tests/testModule.test.ts
+++ server/tests/testModule.test.ts
@@ -1,8 +1,8 @@
-import { add } from './testModule';
+import { add } from '../testModule';
 
 describe.each([
   [1, 1, 2],
-  [4, -2, 2],
+  [4, -2, 1],
   [-12, -34, -46],
 ])('add(%i, %i)', (a, b, expected) => {
   test(`足したら${expected}になるはずだぞぉ？`, () => {
