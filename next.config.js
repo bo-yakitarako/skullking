6bb07ba12acb69a120d4bc5b5c50d7497c5221f1
bo-yakitarako <55777096+bo-yakitarako@users.noreply.github.com>
2021-10-24 02:20:43 +0900

    【環境構築】Dockerとnginxをがんばるくん (#20)
    * はじめてのdocker-compose
    
    * ちょっときついっす
    
    * なんかできて草
    
    * なんか色々
    
    * じゃあURLはこれでいいな
    
    Co-authored-by: bo-yakitarako <bo-yakitarako@users.noreply.github.com>

--- next.config.js
+++ next.config.js
@@ -2,4 +2,13 @@
 module.exports = {
   reactStrictMode: true,
   distDir: '../.next',
+  webpackDevMiddleware: (config) => {
+    config.watchOptions = {
+      aggregateTimeout: 300,
+      poll: 1000,
+      ignored: ['node_modules'],
+    };
+
+    return config;
+  },
 };
