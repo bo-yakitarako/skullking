24fc3abd88909907da95ea42bb8a7a02d9841f30
bo-yakitarako <bo-yakitarako@users.noreply.github.com>
2021-10-24 03:00:35 +0900

    うわあああああ

--- docker/nginx/generate_cert.sh
+++ docker/nginx/generate_cert.sh
@@ -12,13 +12,12 @@ openssl x509 -in ca.crt -text;
 openssl x509 -inform PEM -outform DER -in ca.crt -out ca.der;
 
 # [証明書作成]
-mkdir ../ssl;
-cd ../ssl;
+cd ../;
 
 openssl genrsa -out server.key 2048;
 openssl req -new -sha256 -key server.key -out server.csr -subj "/C=JP/ST=Skullking/L=Mermaid/O=Skullking Pirates inc./CN=skullking";
 openssl req -noout -in server.csr -text;
 
-yes | openssl ca -config <(cat ../openssl.cnf <(printf "\n[usr_cert]\nsubjectAltName=DNS:skullking,DNS:\*.skullking\nextendedKeyUsage=critical,timeStamping,serverAuth,clientAuth,codeSigning")) -keyfile ../skullkingCA/ca.key -outdir ./ -cert ../skullkingCA/ca.crt -in server.csr -out server.crt -days 820 -md sha256;
+yes | openssl ca -config <(cat ./openssl.cnf <(printf "\n[usr_cert]\nsubjectAltName=DNS:skullking,DNS:\*.skullking\nextendedKeyUsage=critical,timeStamping,serverAuth,clientAuth,codeSigning")) -keyfile ./skullkingCA/ca.key -outdir ./ -cert ./skullkingCA/ca.crt -in server.csr -out server.crt -days 820 -md sha256;
 
 openssl x509 -in server.crt -text;
