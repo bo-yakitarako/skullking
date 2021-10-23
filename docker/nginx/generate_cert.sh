b8a0210df37b6edd067cdcbf74aaf7d021c94e16
bo-yakitarako <bo-yakitarako@users.noreply.github.com>
2021-10-24 02:52:41 +0900

    スクリプト書き直した

--- docker/nginx/generate_cert.sh
+++ docker/nginx/generate_cert.sh
@@ -19,6 +19,6 @@ openssl genrsa -out server.key 2048;
 openssl req -new -sha256 -key server.key -out server.csr -subj "/C=JP/ST=Skullking/L=Mermaid/O=Skullking Pirates inc./CN=skullking";
 openssl req -noout -in server.csr -text;
 
-yes | openssl ca -config <(cat /usr/lib/ssl/openssl.cnf <(printf "\n[usr_cert]\nsubjectAltName=DNS:skullking,DNS:\*.skullking\nextendedKeyUsage=critical,timeStamping,serverAuth,clientAuth,codeSigning")) -keyfile ~/skullkingCA/ca.key -outdir ./ -cert ~/skullkingCA/ca.crt -in server.csr -out server.crt -days 820 -md sha256;
+yes | openssl ca -config <(cat ../openssl.cnf <(printf "\n[usr_cert]\nsubjectAltName=DNS:skullking,DNS:\*.skullking\nextendedKeyUsage=critical,timeStamping,serverAuth,clientAuth,codeSigning")) -keyfile ../skullkingCA/ca.key -outdir ./ -cert ../skullkingCA/ca.crt -in server.csr -out server.crt -days 820 -md sha256;
 
 openssl x509 -in server.crt -text;
